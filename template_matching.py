8d39cebbc31eea4a89d8bd263d6286613a0f9524
Tokareva Kseniia <st068737@student.spbu.ru>
2022-03-22 13:08:56 +0300

    Add the face detectors
    Template matching, Viola-Johnes

---
+++ template_matching.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+PATH_PIC = 'test/7_cloaked_high.png'
+PATH_T = 'test/t1.jpg'
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv.imread(PATH_PIC, 0)
+img2 = img.copy()
+template = cv.imread(PATH_T, 0)
+w, h = template.shape[::-1]
+# All the 6 methods for comparison in a list
+methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF',
+           'cv.TM_SQDIFF_NORMED']
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+    # Apply template Matching
+    res = cv.matchTemplate(img, template, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img, top_left, bottom_right, 255, 2)
+    plt.subplot(121), plt.imshow(template, cmap='gray')
+    plt.title('Template'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(img, cmap='gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+    plt.show()
